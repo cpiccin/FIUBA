4cb19a05fa7708b2d1e26ca5b13d2aa970d5387f
candela <cpiccin@gmail.com>
2024-05-30 17:54:24 -0300

    sssss

--- Organizacion del Computador/Tp - El Zorro y las Ocas/main.asm
+++ Organizacion del Computador/Tp - El Zorro y las Ocas/main.asm
@@ -4,12 +4,11 @@ extern fopen
 extern fclose
 extern fgets 
 extern printf
+extern atoi
 
 section .data           
     ocasCapturadas          dw 0; empieza en 0
-
-    prueba                  db "hola",0
-    
+   
     msgGanoZorro            db "Partida finalizada. Gano el zorro",10,0
     msgGanoOcas             db "Partida finalizada. Ganaron las ocas",10,0
 
@@ -17,74 +16,127 @@ section .data
     modo                    db "r",0
 
     format                  db "%s",0
+    formatInt               db "%lli", 0
 
 section .bss
     handlePartida           resq 1
     tablero                 times 50 resb 1 ; tablero 7x7 + null byte 
-    estadisticas            times 9 resw 1 ; vector de estadisticas: [arr, abj, izq, der, diagArrIzq, diagArrDer, diagAbjIzq, diagAbjDer]
-    turnoDe                 resd 1 ; turno actual
+
+    cantMovArr              resd 1
+    cantMovAbj              resd 1
+    cantMovIzq              resd 1
+    cantMovDer              resd 1
+    cantMovDiagIzqArr       resd 1
+    cantMovDiagIzqAbj       resd 1
+    cantMovDiagDerArr       resd 1
+    cantMovDiagDerAbj       resd 1
+
+    turnoDe                 resb 2 ; turno actual
 
 section .text
 main: 
     call manejoDeArchivo 
+    sub rsp, 8
+
+    mov rdi, format 
+    mov rsi, tablero
+    call printf
+
+    mov rdi, format 
+    mov rsi, turnoDe
+    call printf
+
+    mov rdi, formatInt
+    mov rsi, cantMovArr
+    call printf 
+    ; mov rdi, format
+    ; mov rsi, cantMovAbj
+    ; call printf    
+    ; mov rdi, format
+    ; mov rsi, cantMovIzq
+    ; call printf    
+    ; mov rdi, format
+    ; mov rsi, cantMovDer
+    ; call printf    
+    ; mov rdi, format
+    ; mov rsi, cantMovDiagIzqArr
+    ; call printf    
+    ; mov rdi, format
+    ; mov rsi, cantMovDiagIzqAbj
+    ; call printf    
+    ; mov rdi, format
+    ; mov rsi, cantMovDiagDerArr
+    ; call printf
+    ; mov rdi, format
+    ; mov rsi, cantMovDiagDerAbj
+    ; call printf
 
 
+    add rsp, 8
+
+    ret
 
 manejoDeArchivo: 
     ; abrir archivo
+    sub rsp, 8
     mov rdi, filePartida
     mov rsi, modo 
-    sub rsp, 8
     call fopen
-    add rsp, 8
     mov [handlePartida], rax 
 
     ;leo tablero  
     mov rdi, tablero
-    mov rsi, 50
+    mov rsi, 200
     mov rdx, [handlePartida]
-    sub rsp, 8
     call fgets 
-    add rsp, 8
-    ; ;leo estadisticas
-    ; mov rdi, estadisticas
-    ; mov rsi, 8
+
+    ;leo turnoDe
+    mov rdi, turnoDe
+    mov rsi, 200
+    mov rdx, [handlePartida]
+    call fgets 
+
+    ;leo estadisticas
+    mov rdi, r8
+    mov rsi, 200
+    mov rdx, [handlePartida]
+    call fgets
+    mov rdi, r8
+    call atoi
+    mov [cantMovArr], rax
+    ; mov rdi, cantMovAbj
+    ; mov rsi, 200
     ; mov rdx, [handlePartida]
-    ; sub rsp, 8
-    ; call fgets 
-    ; add rsp, 8
-    ; ;leo turnoDe
-    ; mov rdi, turnoDe
-    ; mov rsi, 1
+    ; call fgets
+    ; mov rdi, cantMovIzq
+    ; mov rsi, 200
     ; mov rdx, [handlePartida]
-    ; sub rsp, 8
-    ; call fgets 
-    ; add rsp, 8
+    ; call fgets
+    ; mov rdi, cantMovDer
+    ; mov rsi, 200
+    ; mov rdx, [handlePartida]
+    ; call fgets
+    ; mov rdi, cantMovDiagIzqArr
+    ; mov rsi, 200
+    ; mov rdx, [handlePartida]
+    ; call fgets
+    ; mov rdi, cantMovDiagIzqAbj
+    ; mov rsi, 200
+    ; mov rdx, [handlePartida]
+    ; call fgets
+    ; mov rdi, cantMovDiagDerArr
+    ; mov rsi, 200
+    ; mov rdx, [handlePartida]
+    ; call fgets
+    ; mov rdi, cantMovDiagDerAbj
+    ; mov rsi, 200
+    ; mov rdx, [handlePartida]
+    ; call fgets
+
+
 
     mov rdi,[handlePartida]
-    sub rsp, 8
     call fclose 
     add rsp, 8
     
-    mov rdi, format ; pointer to format string
-    mov rsi, tablero ; pointer to string to print
-    sub rsp, 8 ; align stack
-    call printf
-    add rsp, 8 ; restore stack
-
-    ; ; print estadisticas
-    ; ; note: this assumes that estadisticas is a null-terminated string
-    ; mov rdi, format ; pointer to format string
-    ; mov rsi, estadisticas ; pointer to string to print
-    ; sub rsp, 8 ; align stack
-    ; call printf
-    ; add rsp, 8 ; restore stack
-
-    ; ; print turnoDe
-    ; ; note: this assumes that turnoDe is a null-terminated string
-    ; mov rdi, format ; pointer to format string
-    ; mov rsi, turnoDe ; pointer to string to print
-    ; sub rsp, 8 ; align stack
-    ; call printf
-    ; add rsp, 8 ; restore stack
     ret
